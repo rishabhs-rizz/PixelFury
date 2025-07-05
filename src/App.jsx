2474ba6f98bf7f84f66f2c59108bdc6050d4cb7e
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-07-05 15:09:31 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./Components/Hero";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden text-blue-50">
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
