d8d9edf6808d98cb0d71744f248b8606cc8eea22
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-07-05 18:04:16 +0530

    completed hero section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden text-blue-50">
       <Hero />
+
+      <section className="z-0 min-h-screen bg-blue-500" />
     </main>
   );
 };
