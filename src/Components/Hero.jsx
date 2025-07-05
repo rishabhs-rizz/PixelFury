2dfe48d26286ef38c7a8054a88fd84a1e396a708
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-07-05 15:34:18 +0530

    added herotext

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -69,6 +69,19 @@ const Hero = () => {
             onLoadedData={handleVideoLoad}
           ></video>
         </div>
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75 ">
+          G<b>a</b>ming
+        </h1>
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100 ">
+              redefi<b>n</b>e
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame Layer <br /> Unleash the Play Economy
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
