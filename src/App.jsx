d2f939023e1cf9b421d377c76c10a15c1b0cac58
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-07-10 12:23:32 +0530

    added about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from "./Components/About";
 import Hero from "./Components/Hero";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden text-blue-50">
       <Hero />
+      <About />
 
       <section className="z-0 min-h-screen bg-blue-500" />
     </main>
