e89285ec3d64797336fc2de2e95b2a374b21e1a3
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-07-10 13:05:03 +0530

    animated Title component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { useGSAP } from "@gsap/react";
+import AnimatedTitle from "./AnimatedTitle";
 gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
@@ -28,9 +29,12 @@ const About = () => {
         <h2 className="font-general text-sm uppercase md:text-[10px]">
           Welcome to zentry
         </h2>
-        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-          Disc<b>o</b>ver the World's <br /> l<b>a</b>rgest shared adventure
-        </div>
+        <AnimatedTitle
+          title={
+            "Disc<b>o</b>ver the World's <br /> l<b>a</b>rgest shared adventure"
+          }
+          containerClass="mt-5 !text-black text-center"
+        />
 
         <div className="about-subtext">
           <p>The Game of Games Begins-your life, now an epic MMORPG</p>
---
+++ src/Components/AnimatedTitle.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const AnimatedTitle = ({ title, containerClass }) => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const titleanimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "100 bottom",
+          end: "center bottom",
+          toggleActions: "play none none reverse",
+        },
+      });
+      titleanimation.to(".animated-word", {
+        opacity: 1,
+        transform: "translate3d(0, 0, 0) rotateX(0deg) rotateY(0deg)",
+        stagger: 0.02,
+        ease: "power2.inOut",
+      });
+    }, containerRef);
+
+    return () => {
+      ctx.revert();
+    };
+  }, []);
+  return (
+    <div ref={containerRef} className={`animated-title ${containerClass} `}>
+      {title.split("<br />").map((line, index) => (
+        <div
+          key={index}
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+        >
+          {line.split(" ").map((word, wordIndex) => (
+            <span
+              key={wordIndex}
+              className="animated-word"
+              dangerouslySetInnerHTML={{ __html: word }}
+            ></span>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnimatedTitle;
