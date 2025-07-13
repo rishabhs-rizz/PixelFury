775c8e393e029811cda6d6433bd5e8a0f65efb65
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-07-13 15:07:14 +0530

    fixed bento grid

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -54,7 +54,7 @@ const Features = () => {
           />
         </div>
         <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7 ">
-          <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+          <div className="bento-tilt_1 row-span-1 md:col-span-1 col-span-2 md:row-span-2">
             <BentoCard
               src="videos/feature-2.mp4"
               title={
@@ -66,7 +66,7 @@ const Features = () => {
             />
           </div>
 
-          <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+          <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 col-span-2 md:ms-0">
             <BentoCard
               src="videos/feature-3.mp4"
               title={
@@ -78,7 +78,7 @@ const Features = () => {
             />
           </div>
 
-          <div className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+          <div className="bento-tilt_1 me-14 md:col-span-1 col-span-2 md:me-0">
             <BentoCard
               src="videos/feature-4.mp4"
               title={
@@ -91,8 +91,8 @@ const Features = () => {
           </div>
 
           <div className="bento-tilt_2">
-            <div className="flex size-full flex-col justify-between bg-violet-100 p-5">
-              <h1 className="bento-tile special-font max-w-64">
+            <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+              <h1 className="bento-title special-font max-w-64">
                 M<b>o</b>re co<b>m</b>ing s<b>o</b>on
               </h1>
               <TiLocationArrow className="m-5 scale-[5] self-end" />
