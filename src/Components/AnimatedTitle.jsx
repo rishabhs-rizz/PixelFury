cc3f6bda5d0c7134b8802f0f1a998803f6ec92e7
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-07-14 12:27:34 +0530

    bit of rectification

--- src/Components/AnimatedTitle.jsx
+++ src/Components/AnimatedTitle.jsx
@@ -30,7 +30,7 @@ const AnimatedTitle = ({ title, containerClass }) => {
   }, []);
   return (
     <div ref={containerRef} className={`animated-title ${containerClass} `}>
-      {title.split("<br />").map((line, index) => (
+      {title.split("<br/>").map((line, index) => (
         <div
           key={index}
           className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
